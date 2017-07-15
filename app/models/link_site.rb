af045c8807fdb1c50ad736cf5e04952abd2d3173
Mithridates01 <jclucas21@gmail.com>
2017-07-15 15:08:39 -0400

    Add validations for models

--- app/models/link_site.rb
+++ app/models/link_site.rb
@@ -1,4 +1,7 @@
 class LinkSite < ApplicationRecord
   belongs_to :link
   belongs_to :site
+
+  validates_presence_of :link_id, :site_id
+
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -2,5 +2,7 @@ class Site < ApplicationRecord
   has_many :link_sites
   has_many :links, through: :link_sites
   has_many :contents
-  
+
+  validates_presence_of :url
+
 end
