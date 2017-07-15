1da546ddb74f48d75e8d55c52b6034474b8a0b43
Mithridates01 <jclucas21@gmail.com>
2017-07-15 14:15:58 -0400

    Fix migration error

--- db/migrate/20170715172840_create_link_sites.rb
+++ db/migrate/20170715172840_create_link_sites.rb
@@ -2,7 +2,7 @@ class CreateLinkSites < ActiveRecord::Migration[5.0]
   def change
     create_table :link_sites do |t|
       t.integer :link_id
-      t.d :site_id
+      t.integer :site_id
 
       t.timestamps
     end
